cd86386e2098d94ccc901918ce533852c41abcc5
Omar Ahmed <abdi.omar@riarauniversity.ac.ke>
2025-08-27 09:18:38 +0300

    updatedd

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import landing_main, home_view
+from main.views import landing_main, home_view, list_view
 
 
 
 urlpatterns = [
     path('', landing_main, name='main'),
     path('home/', home_view, name='home'), 
+    path('list/', list_view, name='list')
 ]
